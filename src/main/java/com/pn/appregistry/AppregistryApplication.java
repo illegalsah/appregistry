85ebcaf96a2133f545bdf0a33293034abf578ee9
padmanabha <spadm22@gmail.com>
2024-08-14 22:01:15 +0530

    initial service registry

---
+++ src/main/java/com/pn/appregistry/AppregistryApplication.java
@@ -0,0 +1,13 @@
+package com.pn.appregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppregistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pn/appregistry/AppregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pn.appregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
